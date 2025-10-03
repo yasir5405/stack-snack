3d1a738d775d1693878d3d8e0c9404489cf0c712
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-03 17:47:10 +0530

    fixed unused imports

--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -42,18 +42,15 @@ const AuthForm = <T extends FieldValues>({
     defaultValues: defaultValues as DefaultValues<T>,
   });
 
-  const handleSubmit: SubmitHandler<T> = async () => {
-    // TODO: Authenticate User
-  };
+  // const handleSubmit: SubmitHandler<T> = async () => {
+  //   // TODO: Authenticate User
+  // };
 
   const buttonText = formType === "SIGN_IN" ? "Sign In" : "Sign Up";
 
   return (
     <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(handleSubmit)}
-        className="mt-10 space-y-6"
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)} className="mt-10 space-y-6">
         {Object.keys(defaultValues).map((field) => (
           <FormField
             key={field}
