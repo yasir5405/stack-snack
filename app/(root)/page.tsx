b741f6ea632182cc7acc58438e634e3778d8735f
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-06 04:46:20 +0530

    Implement local search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,65 @@
 import LogoutButton from "@/components/LogoutButton";
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/route";
 import Link from "next/link";
 
-const Page = async () => {
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "Javascript" },
+    ],
+    author: { _id: "1", name: "Yasir Naseem" },
+    upvotes: 10,
+    answers: 4,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "Best practices for TypeScript in Next.js",
+    description:
+      "What are some recommended patterns for using TypeScript with Next.js?",
+    tags: [
+      { _id: "3", name: "TypeScript" },
+      { _id: "4", name: "Next.js" },
+    ],
+    author: { _id: "2", name: "Alex Kim" },
+    upvotes: 15,
+    answers: 2,
+    views: 80,
+    createdAt: new Date(),
+  },
+  {
+    _id: "3",
+    title: "How to style components in Tailwind CSS",
+    description:
+      "How do you organize and reuse styles with Tailwind CSS in a React project?",
+    tags: [
+      { _id: "5", name: "Tailwind CSS" },
+      { _id: "1", name: "React" },
+    ],
+    author: { _id: "3", name: "Priya Singh" },
+    upvotes: 7,
+    answers: 3,
+    views: 60,
+    createdAt: new Date(),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Page = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
   return (
     <>
       <section className="w-full flex flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -16,16 +72,21 @@ const Page = async () => {
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-
-      <section className="mt-11">LocalSearch</section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
       HomeFilter
-      
       <div className="mt-10 flex w-full flex-col gap-6">
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
       </div>
+      <LogoutButton />
     </>
   );
 };
---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Image from "next/image";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/url";
+
+interface Props {
+  route: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: searchQuery,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["query"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 1000);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery, route, router, searchParams, pathname]);
+  return (
+    <div
+      className={cn(
+        "background-light800_darkgradient flex  min-h-[56px] grow items-center gap-4 rounded-[10px] px-4",
+        otherClasses
+      )}
+    >
+      <Image
+        src={imgSrc}
+        height={24}
+        width={24}
+        alt="Search"
+        className="cursor-pointer"
+      />
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="paragraph-regular no-focus placeholder !text-dark400_light700 border-none shadow-none outline-none !bg-transparent dark:!bg-transparent focus:!bg-transparent dark:focus:!bg-transparent"
+      />
+    </div>
+  );
+};
+
+export default LocalSearch;
