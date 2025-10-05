c0ca8b720ca1313816eb1b47a73cebd3f95db014
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-06 03:23:18 +0530

    Implement structure of home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,32 @@
-import { authOptions } from "@/auth";
 import LogoutButton from "@/components/LogoutButton";
-import { getServerSession } from "next-auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/route";
+import Link from "next/link";
 
 const Page = async () => {
-  const session = await getServerSession(authOptions);
-  console.log(session);
-
   return (
-    <div className="">
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of NextJS
-      </h1>
-      {/* <h1>{JSON.stringify(session)}</h1> */}
-      <LogoutButton />
-    </div>
+    <>
+      <section className="w-full flex flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
+    </>
   );
 };
 
