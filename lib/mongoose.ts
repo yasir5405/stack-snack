a419dba0f42080d4d8c718565dd73a27a63e31b6
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-07 16:52:32 +0530

    Add mongodb and mongoose

---
+++ lib/mongoose.ts
@@ -0,0 +1,48 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined.");
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "stack-snack",
+      })
+      .then((result) => {
+        console.log("Connected to MongoDB.");
+        return result;
+      })
+      .catch((error) => {
+        console.log("Error connecting to MongoDB: ", error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default dbConnect;
