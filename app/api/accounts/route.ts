36b891764300491edbe3e80c54f3b01a40119936
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-08 04:26:24 +0530

    Implement create and read account routes

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,50 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const accounts = await Account.find();
+
+    return NextResponse.json(
+      { success: true, data: accounts },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    await dbConnect();
+
+    const body = await req.json();
+
+    const validatedData = AccountSchema.parse(body);
+
+    const existingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+
+    if (existingAccount)
+      throw new ForbiddenError(
+        "An account with the same provider already exists."
+      );
+
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
