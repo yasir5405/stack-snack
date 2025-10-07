e574d57d008fc266f80ce30fff01a73a57d427e0
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-07 18:17:09 +0530

    Add tag and tag-question Model

--- database/template.model.ts
+++ database/template.model.ts
@@ -4,6 +4,6 @@
 
 // const ModelSchema = new Schema<IModel>({}, { timestamps: true });
 
-// const Model = models?.model || model<IModel>("Model", ModelSchema);
+// const Model = models?.Model || model<IModel>("Model", ModelSchema);
 
 // export default Model;
