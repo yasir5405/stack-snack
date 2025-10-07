711363d65b6b1d8ce1b4bb0d149346d0fcb7977d
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-07 20:18:56 +0530

    Implement logging

--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { RequestError, ValidationError } from "../http-errors";
 import { ZodError } from "zod";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -25,6 +26,12 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      {
+        err: error,
+      },
+      `${responseType.toUpperCase()} Error: ${error.message}`
+    );
     return formatResponse(
       responseType,
       error.statusCode,
@@ -37,6 +44,12 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
     const validationError = new ValidationError(
       error.flatten().fieldErrors as Record<string, string[]>
     );
+
+    logger.error(
+      { err: error },
+      `Validation Error: ${validationError.message}`
+    );
+
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -46,9 +59,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
     return formatResponse(responseType, 500, error.message);
   }
 
+  logger.error({ err: error }, "An unexpected error occurred.");
   return formatResponse(responseType, 500, "An unexpected error occurred.");
 };
 
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,4 +1,5 @@
 import mongoose, { Mongoose } from "mongoose";
+import logger from "./logger";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
@@ -23,6 +24,7 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info("Using existing mongoose connection.");
     return cached.conn;
   }
 
@@ -32,11 +34,11 @@ const dbConnect = async (): Promise<Mongoose> => {
         dbName: "stack-snack",
       })
       .then((result) => {
-        console.log("Connected to MongoDB.");
+        logger.info("Connected to MongoDB.");
         return result;
       })
       .catch((error) => {
-        console.log("Error connecting to MongoDB: ", error);
+        logger.error("Error connecting to MongoDB: ", error);
         throw error;
       });
   }
