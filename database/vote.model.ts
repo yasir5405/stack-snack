f9a6297702ca60530b22ca6bc1d6d222691f168a
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-07 18:25:34 +0530

    Add collection Model

--- database/vote.model.ts
+++ database/vote.model.ts
@@ -7,7 +7,6 @@ export interface IVote {
   voteType: "upvote" | "downvote";
 }
 
-export interface IVoteDoc extends IVote, Document {}
 const VoteSchema = new Schema<IVote>(
   {
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
