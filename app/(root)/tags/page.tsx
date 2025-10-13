f7c5d44625509a9e815d48ac33a47a53f549515c
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-13 18:06:56 +0530

    Implement getTags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,4 +1,16 @@
-const Tags = () => {
+import { getTags } from "@/lib/actions/tag.actions";
+
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "nodejs",
+  });
+
+  const { tags } = data || {};
+
+  console.log("TAGS: ", JSON.stringify(tags, null, 2));
+
   return <div>Tags</div>;
 };
 
--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,3 +1,80 @@
-// export const getTags = async (
-//   params: PaginatedSearchParams
-// ): Promise<ActionResponse<{ tags: Tag[]; isNext: boolean }>> => {};
+"use server";
+import { FilterQuery } from "mongoose";
+import action from "../action";
+import handleError from "../handlers/error";
+import { PaginatedSearchParamsSchema } from "../validations";
+import { Tag } from "@/database";
+
+export const getTags = async (
+  params: PaginatedSearchParams
+): Promise<ActionResponse<{ tags: Tag[]; isNext: boolean }>> => {
+  const validationResult = await action({
+    params,
+    schema: PaginatedSearchParamsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { page = 1, pageSize = 10, query, filter } = params;
+
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = pageSize;
+
+  const filterQuery: FilterQuery<typeof Tag> = {};
+
+  if (query) {
+    filterQuery.$or = [
+      {
+        name: {
+          $regex: query,
+          $options: "i",
+        },
+      },
+    ];
+  }
+
+  let sortCriteria = {};
+  if (sortCriteria) {
+    switch (filter) {
+      case "popular":
+        sortCriteria = { questions: -1 };
+        break;
+      case "recent":
+        filterQuery.answers = 0;
+        sortCriteria = { createdAt: -1 };
+        break;
+      case "oldest":
+        sortCriteria = { createdAt: 1 };
+        break;
+      case "name":
+        sortCriteria = { name: 1 };
+        break;
+      default:
+        sortCriteria = { createdAt: -1 };
+        break;
+    }
+  }
+
+  try {
+    const totalTags = await Tag.countDocuments(filterQuery);
+
+    const tags = await Tag.find(filterQuery)
+      .sort(sortCriteria)
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalTags > skip + tags.length;
+
+    return {
+      success: true,
+      data: {
+        tags: JSON.parse(JSON.stringify(tags)),
+        isNext,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+};
