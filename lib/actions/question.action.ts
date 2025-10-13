ae0e509bc8d93d7aad710adf5b84d672de96fdcb
yasir5405 <yasirnaseem5415@gmail.com>
2025-10-13 21:50:57 +0530

    implement increment question views

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -5,6 +5,7 @@ import {
   AskQuestionSchema,
   EditQuestionSchema,
   GetQuestionSchema,
+  IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
 import handleError from "../handlers/error";
@@ -299,6 +300,40 @@ export async function getQuestions(params: PaginatedSearchParams): Promise<
       data: {
         questions: JSON.parse(JSON.stringify(questions)),
         isNext,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function incrementViews(
+  params: IncrementViewsParams
+): Promise<ActionResponse<{ views: number }>> {
+  const validationResult = await action({
+    params,
+    schema: IncrementViewsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = params;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) throw new Error("Question not found");
+
+    question.views += 1;
+
+    await question.save();
+
+    return {
+      success: true,
+      data: {
+        views: question.views,
       },
     };
   } catch (error) {
--- types/action.d.ts
+++ types/action.d.ts
@@ -33,3 +33,7 @@ interface GetQuestionParams {
 interface GetTagQuestionsParams extends Omit<PaginatedSearchParams, "filter"> {
   tagId: string;
 }
+
+interface IncrementViewsParams {
+  questionId: string;
+}
