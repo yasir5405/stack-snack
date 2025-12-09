e2ad5555094ff8c628b3295e9a4d4ded1d8ceee9
yasir5405 <yasirnaseem5415@gmail.com>
2025-12-09 21:11:54 +0530

    Implement Profile Page Header

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,5 +1,96 @@
-const Profile = () => {
-  return <div>Profile</div>;
+import { authOptions } from "@/auth";
+import ProfileLink from "@/components/user/ProfileLink";
+import UserAvatar from "@/components/UserAvatar";
+import { getUser } from "@/lib/actions/user.action";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import dayjs from "dayjs";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const Profile = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  if (!id) notFound();
+
+  const loggedInUser = await getServerSession(authOptions);
+
+  const { success, data, error } = await getUser({
+    userId: id,
+  });
+
+  if (!success)
+    return (
+      <div>
+        <div className="h1-bold text-dark100_light900">{error?.message}</div>
+      </div>
+    );
+
+  const { totalAnswers, totalQuestions, user } = data!;
+  const {
+    _id,
+    email,
+    name,
+    username,
+    image,
+    createdAt,
+    bio,
+    location,
+    portfolio,
+    reputation,
+  } = user;
+
+  return (
+    <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+      <div className="flex flex-col items-start gap-4 lg:flex-row">
+        <UserAvatar
+          name={name}
+          id={_id}
+          imageUrl={image}
+          className="size-[140px] rounded-full object-cover"
+          fallbackClassName="text-6xl font-bolder"
+        />
+
+        <div className="mt-3">
+          <h1 className="h2-bold text-dark100_light900">{name}</h1>
+          <p className="paragraph-regular text-dark200_light800">@{username}</p>
+          <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+            {portfolio && (
+              <ProfileLink
+                imgUrl="/icons/link.svg"
+                href={portfolio}
+                title="Portfolio"
+              />
+            )}
+            {location && (
+              <ProfileLink imgUrl="/icons/location.svg" title="Location" />
+            )}
+
+            <ProfileLink
+              imgUrl="/icons/calendar.svg"
+              href={portfolio}
+              title={dayjs(createdAt).format("MMMM YYYY")}
+            />
+          </div>
+
+          {bio && (
+            <p className="paragraph-regular text-dark400_light800 mt-8">
+              {bio}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+        {loggedInUser?.user.id === id && (
+          <Link href="/profile/edit">
+            <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
+              Edit Profile
+            </Button>
+          </Link>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
