e93aec96c383098111754c2bb1f8acdf30cecc26
yasir5405 <yasirnaseem5415@gmail.com>
2025-12-11 20:42:45 +0530

    Add og image for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,85 +1,15 @@
-// import HomeFilter from "@/components/filters/HomeFilter";
-// import Pagination from "@/components/Pagination";
-// import QuestionsList from "@/components/questions/QuestionsList";
-// import LocalSearch from "@/components/search/LocalSearch";
-// import QuestionCardSkeleton from "@/components/skeletons/QuestionCardSkeleton";
-// import { getQuestions } from "@/lib/actions/question.action";
-// import { Suspense } from "react";
-
-// interface SearchParams {
-//   searchParams: Promise<{ [key: string]: string }>;
-// }
-
-// const Page = async ({ searchParams }: SearchParams) => {
-//   const { query, filter, page, pageSize } = await searchParams;
-
-//   const parsedPage = Number(page) || 1;
-//   const parsedPageSize = Number(pageSize) || 10;
-
-//   // Get isNext for pagination
-//   const { data } = await getQuestions({
-//     filter: filter || "",
-//     page: parsedPage,
-//     pageSize: parsedPageSize,
-//     query: query || "",
-//   });
-
-//   const { isNext } = data || {};
-
-//   // Create unique key for Suspense boundary
-//   const questionsKey = `questions-${filter || "all"}-${query || "none"}-${parsedPage}`;
-
-//   return (
-//     <>
-//       <section className="w-full flex flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-//         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-//         {/* Ask Question Button */}
-//       </section>
-
-//       <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-//         <LocalSearch
-//           route="/"
-//           imgSrc="/icons/search.svg"
-//           placeholder="Search questions..."
-//           otherClasses="flex-1"
-//         />
-//         {/* Mobile Filter */}
-//       </section>
-
-//       <HomeFilter />
-
-//       {/* Suspense boundary with dynamic key to trigger loading on filter change */}
-//       <Suspense
-//         key={questionsKey}
-//         fallback={
-//           <div className="mt-10 flex w-full flex-col gap-6">
-//             {[1, 2, 3, 4, 5].map((i) => (
-//               <QuestionCardSkeleton key={i} />
-//             ))}
-//           </div>
-//         }
-//       >
-//         <QuestionsList
-//           filter={filter || ""}
-//           query={query || ""}
-//           page={parsedPage}
-//           pageSize={parsedPageSize}
-//         />
-//       </Suspense>
-
-//       <Pagination page={parsedPage} isNext={isNext || false} />
-//     </>
-//   );
-// };
-
-// export default Page;
-
 import HomeFilter from "@/components/filters/HomeFilter";
-import Pagination from "@/components/Pagination";
 import QuestionsList from "@/components/questions/QuestionsList";
 import LocalSearch from "@/components/search/LocalSearch";
 import QuestionCardSkeleton from "@/components/skeletons/QuestionCardSkeleton";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Dev Overflow | Home",
+  description:
+    "Discover different programming questions and answers with recommendations from the community.",
+};
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -13,11 +13,50 @@ import { hasSavedQuestion } from "@/lib/actions/collection.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { hasVoted } from "@/lib/actions/vote.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
+import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { after } from "next/server";
 import { Suspense } from "react";
+
+export async function generateMetadata({
+  params,
+}: RouteParams): Promise<Metadata> {
+  const { id } = await params;
+
+  const { success, data: question } = await getQuestion({ questionId: id });
+
+  if (!success || !question) {
+    return {
+      title: "Question not found.",
+      description: "This question does not exists.",
+    };
+  }
+
+  return {
+    title: question.title,
+    description: question.content.slice(0, 100),
+    twitter: {
+      card: "summary_large_image",
+      title: question.title,
+      description: question.content.slice(0, 100),
+      images: ["/og/home.png"],
+    },
+    openGraph: {
+      title: question.title,
+      description: question.content.slice(0, 100),
+      images: [
+        {
+          url: "/og/home.png",
+          width: 1200,
+          height: 630,
+          alt: question.title,
+        },
+      ],
+    },
+  };
+}
 
 const QuestionDetails = async ({ params, searchParams }: RouteParams) => {
   const session = await getServerSession(authOptions);
